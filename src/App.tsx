b237d4836cbe533e378c919640cdcffb6ceaa7c7
clovis20 <36241818+clovis20@users.noreply.github.com>
2023-09-04 17:37:51 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import { GlobalCss } from './styles'
+
+import Rotas from './routes'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalCss />
+      <div className="container">
+        <Rotas />
+        <Footer />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
