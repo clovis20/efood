05cb33ce71c80cf0ee762806cf4b68c9c8d3614f
clovis20 <36241818+clovis20@users.noreply.github.com>
2023-09-08 15:40:38 -0300

    second commit

--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -1,47 +1,32 @@
 import styled from 'styled-components'
-import { cores } from '../../styles'
+import { cores } from '../../../styles'
 
 export const Card = styled.div`
-  background-color: ${cores.fundo1};
-  position: relative;
-  border: 1px solid ${cores.laranja};
+  background-color: ${cores.laranja};
+  color: ${cores.branca};
+  border: 8px solid ${cores.laranja};
 `
 
 export const Titulo = styled.h3`
   font-weight: bold;
-  font-size: 18px;
-  display: block;
-  margin-left: 8px;
+  font-size: 16px;
 `
 
 export const Descricao = styled.p`
   font-size: 14px;
   line-height: 22px;
   display: block;
-  margin-top: 16px;
-  margin-bottom: 16px;
-  margin-left: 8px;
+  margin-top: 8px;
+  margin-bottom: 8px;
 `
 
-export const Infos = styled.div`
-  position: absolute;
-  top: 16px;
-  right: 16px;
-`
-
-export const Notas = styled.div`
-  font-size: 18px;
+export const Botao = styled.button`
+  background-color: ${cores.fundo2};
+  color: ${cores.laranja};
+  font-size: 14px;
   font-weight: bold;
-  margin-right: 8px;
-`
-
-export const TituloDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-right: 8px;
-
-  .display {
-    display: flex;
-    }
-  }
+  width: 100%;
+  height: 24px;
+  border: none;
+  cursor: pointer;
 `
