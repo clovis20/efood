f05b9cede7ae3e572bae1dd53f2e01824e7dd91d
clovis20 <36241818+clovis20@users.noreply.github.com>
2023-09-08 15:41:02 -0300

    second commit

--- src/components/Trattoria/ProductsList/styles.ts
+++ src/components/Trattoria/ProductsList/styles.ts
@@ -2,15 +2,18 @@ import styled from 'styled-components'
 import { cores } from '../../../styles'
 
 export const Container = styled.section`
-  padding: 80px 0;
   background-color: ${cores.fundo1};
 `
 
 export const List = styled.ul`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-row-gap: 32px;
-  column-gap: 40px;
-  max-width: 1024px;
-  margin: 0 auto;
+  gap: 32px;
+  max-width: 1366px;
+  padding-top: 56px;
+  padding-bottom: 120px;
+  padding-left: 171px;
+  padding-right: 171px;
+  width: 100%;
+  margin: auto;
 `
