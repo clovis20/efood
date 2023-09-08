4211e4ee439b8a4cfff51bee31a56c7a748e2d20
clovis20 <36241818+clovis20@users.noreply.github.com>
2023-09-08 15:41:28 -0300

    second commit

--- src/components/Trattoria/Vita/styles.ts
+++ src/components/Trattoria/Vita/styles.ts
@@ -1,14 +1,16 @@
 import styled from 'styled-components'
 import { cores } from '../../../styles'
 
-export const Restbar = styled.header`
+export const Restbar = styled.div`
   font-size: 26px;
   height: 280px;
   color: ${cores.branca};
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  padding: 33px 101px;
+  padding: 33px 171px;
+  max-width: 1366px;
+  margin: auto;
 
   h2 {
     font-weight: bold;
