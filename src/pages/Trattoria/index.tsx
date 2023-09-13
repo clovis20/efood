d6ae01f21a83517d7756137e1d07b799dccd7db1
clovis20 <36241818+clovis20@users.noreply.github.com>
2023-09-13 11:37:59 -0300

    third commit

--- src/pages/Trattoria/index.tsx
+++ src/pages/Trattoria/index.tsx
@@ -1,67 +1,54 @@
+import { useEffect, useState } from 'react'
 import ProductsList from '../../components/Trattoria/ProductsList'
-import Cardapio from '../../models/Cardapio'
-
-import pizza from '../../assets/images/pizza.png'
 import Header from '../../components/Trattoria/Header'
 import Vita from '../../components/Trattoria/Vita'
 
-const cardapio: Cardapio[] = [
-  {
-    id: 1,
-    button: 'Adicionar ao carrinho',
-    description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
-    title: 'Pizza Marguerita',
-    image: pizza
-  },
-  {
-    id: 2,
-    button: 'Adicionar ao carrinho',
-    description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
-    title: 'Pizza Marguerita',
-    image: pizza
-  },
-  {
-    id: 3,
-    button: 'Adicionar ao carrinho',
-    description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
-    title: 'Pizza Marguerita',
-    image: pizza
-  },
-  {
-    id: 4,
-    button: 'Adicionar ao carrinho',
-    description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
-    title: 'Pizza Marguerita',
-    image: pizza
-  },
-  {
-    id: 5,
-    button: 'Adicionar ao carrinho',
-    description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
-    title: 'Pizza Marguerita',
-    image: pizza
-  },
-  {
-    id: 6,
-    button: 'Adicionar ao carrinho',
-    description:
-      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
-    title: 'Pizza Marguerita',
-    image: pizza
-  }
-]
+export interface Prato {
+  id: number
+  foto: string
+  preco: number
+  nome: string
+  descricao: string
+  porcao: string
+}
 
-const Trattoria = () => (
-  <>
-    <Header />
-    <Vita />
-    <ProductsList pratos={cardapio} title="Pratos" />
-  </>
-)
+interface Restaurante {
+  id: number
+  titulo: string
+  descricao: string
+  capa: string
+  cardapio: Prato[]
+}
+
+const Trattoria = () => {
+  const [cardapio, setCardapio] = useState<Prato[]>([])
+
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/efood/restaurantes')
+      .then((res) => res.json())
+      .then((res: Restaurante[]) => {
+        // Filtrar o restaurante com ID 5
+        const restaurante = res.find((rest: Restaurante) => rest.id === 5)
+
+        if (restaurante) {
+          // Encontrar o prato com ID 17 dentro do restaurante
+          const prato = restaurante.cardapio.find((item) => item.id === 17)
+
+          if (prato) {
+            const pratosRepetidos = Array(6).fill(prato)
+            setCardapio(pratosRepetidos)
+          }
+        }
+      })
+  }, [])
+
+  return (
+    <>
+      <Header />
+      <Vita />
+      <ProductsList pratos={cardapio} />
+    </>
+  )
+}
 
 export default Trattoria
