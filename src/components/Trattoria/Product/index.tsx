4e92c76a7caba7d31a1a92a669f3fe76baabc894
clovis20 <36241818+clovis20@users.noreply.github.com>
2023-09-13 11:41:50 -0300

    third commit

--- src/components/Trattoria/Product/index.tsx
+++ src/components/Trattoria/Product/index.tsx
@@ -1,21 +1,107 @@
-import { Card, Descricao, Titulo, Botao } from './styles'
+import { useState } from 'react'
+import fechar from '../../../assets/images/fechar.png'
+import {
+  Card,
+  Descricao,
+  Titulo,
+  Botao,
+  Modal,
+  ModalContent,
+  AdicionarBotao
+} from './styles'
 
-type Props = {
-  title: string
-  button: string
-  description: string
-  image: string
+interface Prato {
+  id: number
+  foto: string
+  preco: number
+  nome: string
+  descricao: string
+  porcao: string
 }
 
-const Product = ({ title, description, image }: Props) => (
-  <Card>
-    <div>
-      <img src={image} alt={title} />
-    </div>
-    <Titulo>{title}</Titulo>
-    <Descricao>{description}</Descricao>
-    <Botao>Adicionar ao carrinho</Botao>
-  </Card>
-)
+interface ModalState {
+  isVisible: boolean
+  type: 'image'
+  url: string
+}
+
+interface Props {
+  prato: Prato
+}
+
+const Product = ({ prato }: Props) => {
+  const formataPreco = (preco: number) => {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(preco)
+  }
+
+  const [modalEstaAberto, setModalEstaAberto] = useState<ModalState>({
+    isVisible: false,
+    type: 'image',
+    url: ''
+  })
+
+  return (
+    <>
+      <Card>
+        <div>
+          <img src={prato.foto} alt={prato.nome} />
+        </div>
+        <Titulo>{prato.nome}</Titulo>
+        <Descricao>{prato.descricao}</Descricao>
+        <Botao
+          onClick={() => {
+            setModalEstaAberto({
+              isVisible: true,
+              type: 'image',
+              url: ''
+            })
+          }}
+        >
+          Mais detalhes
+        </Botao>
+      </Card>
+      <Modal className={modalEstaAberto.isVisible ? 'visivel' : ''}>
+        <ModalContent className="container">
+          <header>
+            <img
+              src={fechar}
+              className="close-button"
+              alt="Fechar"
+              onClick={() => {
+                setModalEstaAberto({
+                  isVisible: false,
+                  type: 'image',
+                  url: ''
+                })
+              }}
+            />
+          </header>
+          <img src={prato.foto} alt={prato.nome} />
+          <div>
+            <h3>{prato.nome}</h3>
+            <p>{prato.descricao}</p>
+            <p> Serve: {prato.porcao}</p>
+            <AdicionarBotao>
+              Adicionar ao carrinho - {formataPreco(prato.preco)}
+            </AdicionarBotao>
+          </div>
+        </ModalContent>
+        <div
+          className="overlay"
+          onClick={() => {
+            setModalEstaAberto({
+              isVisible: false,
+              type: 'image',
+              url: ''
+            })
+          }}
+        ></div>
+      </Modal>
+    </>
+  )
+}
 
 export default Product
--- src/components/Trattoria/Product/styles.ts
+++ src/components/Trattoria/Product/styles.ts
@@ -5,11 +5,24 @@ export const Card = styled.div`
   background-color: ${cores.laranja};
   color: ${cores.branca};
   border: 8px solid ${cores.laranja};
+
+  > div {
+    max-width: 100%;
+    max-height: 280px;
+    object-fit: cover;
+
+    > img {
+      width: 100%;
+    }
+  }
 `
+
+export const ImagemPrato = styled.img``
 
 export const Titulo = styled.h3`
   font-weight: bold;
   font-size: 16px;
+  margin-top: 8px;
 `
 
 export const Descricao = styled.p`
@@ -17,7 +30,6 @@ export const Descricao = styled.p`
   line-height: 22px;
   display: block;
   margin-top: 8px;
-  margin-bottom: 8px;
 `
 
 export const Botao = styled.button`
@@ -29,4 +41,85 @@ export const Botao = styled.button`
   height: 24px;
   border: none;
   cursor: pointer;
+  margin-top: 8px;
+`
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: none;
+  align-items: center;
+  justify-content: center;
+
+  &.visivel {
+    display: flex;
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.75);
+  }
+`
+
+export const ModalContent = styled.div`
+  position: relative;
+  max-width: 1024px;
+  display: flex;
+  background-color: ${cores.laranja};
+  color: ${cores.branca};
+  text-align: left;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22px;
+  padding: 32px;
+  z-index: 1;
+
+  h3 {
+    font-size: 18px;
+    margin-left: 24px;
+  }
+
+  > img {
+    width: 280px;
+    height: 280px;
+    object-fit: cover;
+  }
+
+  p {
+    margin-left: 24px;
+    margin-right: 32px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+  }
+
+  button {
+    margin-left: 24px;
+  }
+
+  img.close-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+  }
+`
+
+export const AdicionarBotao = styled.button`
+  background-color: ${cores.fundo2};
+  color: ${cores.laranja};
+  font-size: 14px;
+  font-weight: bold;
+  height: 24px;
+  border: none;
+  cursor: pointer;
+  padding-left: 8px;
+  padding-right: 8px;
 `
