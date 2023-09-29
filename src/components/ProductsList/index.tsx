937b16f4ecc5f40188db4f09d31ab8bd2e71f070
clovis20 <36241818+clovis20@users.noreply.github.com>
2023-09-29 10:20:39 -0300

    fourth commit

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,31 +1,57 @@
-import Restaurante from '../../models/Restaurante'
+import { useState, useEffect } from 'react'
 import Product from '../Product'
 
 import { Container, List } from './styles'
 
-export type Props = {
-  title: string
-  restaurantes: Restaurante[]
+type Restaurante = {
+  id: number
+  titulo: string
+  descricao: string
+  tipo: string
+  avaliacao: number
+  capa: string
 }
 
-const ProductsList = ({ restaurantes }: Props) => (
-  <Container>
-    <div className="container">
-      <List>
-        {restaurantes.map((restaurante) => (
-          <Product
-            key={restaurante.id}
-            button={restaurante.button}
-            description={restaurante.description}
-            image={restaurante.image}
-            infos={restaurante.infos}
-            title={restaurante.title}
-            notas={restaurante.notas}
-          />
-        ))}
-      </List>
-    </div>
-  </Container>
-)
+const ProductsList = () => {
+  const [restaurantes, setRestaurantes] = useState<Restaurante[]>([])
+
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/efood/restaurantes')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Não foi possivel buscar os dados da API')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setRestaurantes(data)
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar dados da API', error)
+      })
+  }, [])
+
+  return (
+    <Container>
+      <div className="container">
+        <List>
+          {restaurantes.map((restaurante, index) => (
+            <Product
+              key={restaurante.id}
+              button="Saiba Mais"
+              description={restaurante.descricao}
+              image={restaurante.capa}
+              infos={[restaurante.tipo]}
+              title={restaurante.titulo}
+              notas={restaurante.avaliacao}
+              isDestaque={index === 0}
+              id={restaurante.id}
+            />
+          ))}
+        </List>
+      </div>
+    </Container>
+  )
+}
 
 export default ProductsList
